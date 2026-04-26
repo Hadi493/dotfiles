0ec40ee25773394497c607a9ccb95654329ab6b0
Hadi493 <hadialam493@gmail.com>
2026-04-26 18:46:03 +0600

    bin

--- emacs.conf/.emacs.custom.el
+++ emacs.conf/.emacs.custom.el
@@ -7,7 +7,16 @@
  '(custom-safe-themes
    '("01a9797244146bbae39b18ef37e6f2ca5bebded90d9fe3a2f342a9e863aaa4fd"
      default))
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(ag alert buckwalter chinese-number clojure-mode cmake-mode d-mode
+        dash-functional dockerfile-mode doom-themes elpy glsl-mode
+        go-mode graphviz-dot-mode gruber-darker-theme haskell-mode
+        helm ido-completing-read+ jinja2-mode kotlin-mode lua-mode
+        magit markdown-mode move-text multiple-cursors nginx-mode
+        nim-mode nix-mode org-cliplink php-mode powershell
+        proof-general purescript-mode qml-mode racket-mode rfc-mode
+        rust-mode scala-mode smex sml-mode tide toml-mode tuareg
+        typescript-mode uxntal-mode yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
