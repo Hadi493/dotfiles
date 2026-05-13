fe47ff975f57e3d3ceb13596238bcbc822e12e44
Hadi493 <hadialam493@gmail.com>
2026-05-13 12:10:40 +0600

    changes and fixes

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# --- Dracula Theme Colors ---
 BACKGROUND="282a36"
 SELECTION="44475a"
 FOREGROUND="f8f8f2"
@@ -12,46 +13,57 @@ PURPLE="bd93f9"
 RED="ff5555"
 YELLOW="f1fa8c"
 
+# --- Font Settings ---
+# We use Iosevka SS04 as found on the system earlier
+FONT="Iosevka SS04"
+
 i3lock \
-    --insidever-color=$BACKGROUND'ff' \
-    --ringver-color=$PURPLE'ff' \
-    \
-    --insidewrong-color=$BACKGROUND'ff' \
-    --ringwrong-color=$RED'ff' \
-    \
-    --inside-color=$BACKGROUND'ff' \
-    --ring-color=$SELECTION'ff' \
-    --line-color=$BACKGROUND'00' \
-    --separator-color=$SELECTION'ff' \
-    \
-    --verif-color=$FOREGROUND'ff' \
-    --wrong-color=$RED'ff' \
-    --layout-color=$FOREGROUND'ff' \
-    \
-    --keyhl-color=$GREEN'ff' \
-    --bshl-color=$RED'ff' \
-    \
-    --screen=1 \
-    --blur=9 \
-    --clock \
-    --indicator \
-    --time-str="%I:%M:%S %p" \
-    \
-    --date-str="%A, %d %B %Y" \
-    --time-color=$FOREGROUND'ff' \
-    --date-color=$FOREGROUND'ff' \
-    --radius=250 \
-    --ring-width=10 \
-    --time-font="Iosevka Term SS04" \
-    --date-font="Iosevka Term SS04" \
-    --layout-font="Iosevka SS04" \
-    --verif-font="Iosevka SS04" \
-    --wrong-font="Iosevka SS04" \
-    --greeter-font="Iosevka SS04" \
-    --time-size=70 \
-    --date-size=24 \
-    --layout-size=60 \
-    --verif-size=50 \
-    --wrong-size=50 \
-    --greeter-size=50 \
-    "$@"
+  --color=$BACKGROUND \
+  \
+  --insidever-color=$BACKGROUND'00' \
+  --ringver-color=$CYAN'ff' \
+  \
+  --insidewrong-color=$BACKGROUND'00' \
+  --ringwrong-color=$RED'ff' \
+  \
+  --inside-color=$BACKGROUND'00' \
+  --ring-color=$PURPLE'ff' \
+  --line-color=$BACKGROUND'00' \
+  --separator-color=$SELECTION'ff' \
+  \
+  --verif-color=$CYAN'ff' \
+  --wrong-color=$RED'ff' \
+  --layout-color=$FOREGROUND'ff' \
+  \
+  --keyhl-color=$PINK'ff' \
+  --bshl-color=$ORANGE'ff' \
+  \
+  --screen=1 \
+  --blur=10 \
+  --clock \
+  --indicator \
+  \
+  --time-str="%H:%M" \
+  --time-font="$FONT" \
+  --time-size=120 \
+  --time-color=$FOREGROUND'ff' \
+  \
+  --date-str="%A, %B %d" \
+  --date-font="$FONT" \
+  --date-size=24 \
+  --date-color=$COMMENT'ff' \
+  \
+  --greeter-text="Locked" \
+  --greeter-font="$FONT" \
+  --greeter-size=18 \
+  --greeter-color=$PURPLE'ff' \
+  \
+  --verif-text="Verifying..." \
+  --wrong-text="Try Again" \
+  \
+  --radius=120 \
+  --ring-width=8 \
+  --pass-media-keys \
+  --pass-screen-keys \
+  --pass-volume-keys \
+  "$@"
