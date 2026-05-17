c6ec75365611d54534d30ec8eb05b4d2b19cfc56
Hadi493 <hadialam493@gmail.com>
2026-05-17 07:40:25 +0600

    fix

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -1,4 +1,6 @@
-require('lint').linters_by_ft = { --some of these need to be installed from package manager
+local lint = require('lint')
+
+lint.linters_by_ft = { --some of these need to be installed from package manager
   lua = {'luac'},
   python = {'ruff'},
   sh = {'bash'},
@@ -7,6 +9,21 @@ require('lint').linters_by_ft = { --some of these need to be installed from pack
   css = {'stylelint'},
   html = {'htmlhint'},
 }
+
+-- Filter out linters that are not installed to prevent ENOENT errors
+local filtered_linters = {}
+for ft, linters in pairs(lint.linters_by_ft) do
+  local valid_linters = {}
+  for _, linter in ipairs(linters) do
+    if vim.fn.executable(linter) == 1 then
+      table.insert(valid_linters, linter)
+    end
+  end
+  if #valid_linters > 0 then
+    filtered_linters[ft] = valid_linters
+  end
+end
+lint.linters_by_ft = filtered_linters
 
 -- Some linters require a file to be saved to disk, others support linting stdin input.
 -- For such linters you could also define a more aggressive autocmd,
