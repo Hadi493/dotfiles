0d6fc71832c4d5ec193c5e2bf982824d86d09ccb
Hadi493 <hadialam493@gmail.com>
2026-08-05 11:40:54 +0600

    fix: notification click-to-focus not working in Hyprland

--- .config/hypr/conf/theme.lua
+++ .config/hypr/conf/theme.lua
@@ -58,6 +58,7 @@ hl.config({
         force_default_wallpaper    = 0,
         disable_hyprland_logo      = true,
         disable_splash_rendering   = true,
+        focus_on_activate          = true,
     },
 
     render = {
