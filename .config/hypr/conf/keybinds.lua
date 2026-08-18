7c50a64d26a681930584151736f843a433bcd601
Hadi493 <hadialam493@gmail.com>
2026-08-18 20:17:38 +0600

    fixes and changes

--- .config/hypr/conf/keybinds.lua
+++ .config/hypr/conf/keybinds.lua
@@ -37,7 +37,7 @@ hl.bind("ALT + C", hl.dsp.exec_cmd("kitty peaclock"))
 hl.bind(mainMod .. "+ G", hl.dsp.group.toggle())
 hl.bind("CTRL + left", hl.dsp.group.prev())
 hl.bind("CTRL + right", hl.dsp.group.next())
-hl.bind("CTRL + Tab", hl.dsp.group.prev())
+hl.bind("ALT + Tab", hl.dsp.group.prev())
 
 hl.bind(mainMod .. " + Z", hl.dsp.exec_cmd("coomer"))
 
